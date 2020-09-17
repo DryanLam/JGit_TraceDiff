9445f30c2024a5db01fb63627ca4e8372e032a73
Dryan Lam <dunglam@kms-technology.com>
2020-09-17 18:42:47 +0700

    Wrong ways to find method line but Able to load java file to groovy

--- src/ChangeDetectio.groovy
+++ src/ChangeDetectio.groovy
@@ -1,6 +1,11 @@
-@Grapes(
-        @Grab(group = 'org.eclipse.jgit', module = 'org.eclipse.jgit', version = '5.8.1.202007141445-r')
-)
+import javassist.ClassClassPath
+import javassist.ClassPool
+import javassist.CtClass
+import javassist.LoaderClassPath
+@Grapes([
+        @Grab(group = 'org.eclipse.jgit', module = 'org.eclipse.jgit', version = '5.8.1.202007141445-r'),
+        @Grab(group='org.javassist', module='javassist', version='3.27.0-GA')
+])
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.diff.DiffEntry
@@ -8,6 +13,9 @@ import org.eclipse.jgit.diff.DiffFormatter
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+
+import java.lang.reflect.Method
+
 
 // Alt + Enter at Grapes to add context libs for IntelliJ
 def traceDiff(String pathDir) {
@@ -27,9 +35,10 @@ def traceDiff(String pathDir) {
 
         for (DiffEntry entry : entries) {
             String filename = entry.getNewPath();
-            if (filename.endsWith(".java")) {
+            if (filename =~ /.java$|.jsp$/) {
                 diffFormatter.format(entry);
                 changes += ["file": entry.getNewPath(), "diff": stdout.toString("UTF-8"), "type": entry.getChangeType().toString()]
+//                changes += ["file": entry.getNewPath(), "diff": stdout.toString("UTF-8"), "type": entry.getChangeType().toString()]
             }
         }
         return changes
@@ -48,7 +57,74 @@ def RevCommit getHeadCommit(Repository repository) {
 }
 
 
-// Execute detection
 
-def diffs = traceDiff("H:/Codebase/JGit")
-println(diffs.diff)
+def diffParser(List changes){
+    def diffPattern = /@@(.+?)@@/
+
+    def changeScopes = []
+    changes.each{change ->
+        if("MODIFY" == change.type){
+            def diff = change.diff
+            println(diff)
+            def detachDiff = (diff =~ diffPattern).findAll()*.last()        // [ -7,9 +7,14 ,  -22,13 +27,29 ]
+
+            // Detach changed scopes
+            detachDiff.each{number ->
+                def splitChanges = number.split(',')
+                def indxStart = (splitChanges.first() - "-").toInteger()
+                def changeScope = splitChanges.last().toInteger() - 1   // Git always count to unchanged part
+                changeScopes += ["startLine": indxStart, "endLine": indxStart + changeScope]
+            }
+            def lstDiffs = diff.split('\n')
+
+            println(lstDiffs)
+        }
+    }
+    println(changeScopes)
+}
+
+def loadFile(){
+    def filePath = "H:/Codebase/JGit/src/main/java/com/dl/jgit/CommitTrace.java"
+
+    File file = new File(filePath)
+    ClassLoader classLoader = getClass().getClassLoader()
+    Class loadedClass = classLoader.parseClass(file); // groovySource is a File object pointing to .groovy file
+    def lstMethod = loadedClass.getDeclaredMethods().findAll{it.toString().contains(".dl.")}
+    def value = lstMethod.first().getMetaPropertyValues()
+    println(lstMethod)  // It's hard to detach method with invoke method of java
+
+//    GroovyClassLoader loader = new GroovyClassLoader(parent);
+//    Class groovyClass = loader.parseClass(file); // groovySource is a File object pointing to .groovy file
+//    GroovyObject groovyObject = (GroovyObject) groovyClass.newInstance();
+//    Object[] args = {};
+//    groovyObject.invokeMethod("doit", args);
+
+
+//    File file = new File(filePath)
+//    ClassLoader classLoader = getClass().getClassLoader()
+//    Class loadedClass = classLoader.parseClass(file);
+//    Method m;
+//    ClassPool pool = ClassPool.getDefault();
+//    pool.insertClassPath(new ClassClassPath(loadedClass));
+//    CtClass cc = pool.get("CommitTrace")
+//    println("")
+//    CtClass cc  = classPool.get(filePath)
+
+}
+
+
+//Method m; // the method object
+//ClassPool pool = ClassPool.getDefault();
+//CtClass cc = pool.get(m.getDeclaringClass().getCanonicalName());
+//CtMethod javassistMethod = cc.getDeclaredMethod(m.getName());
+//int linenumber = javassistMethod.getMethodInfo().getLineNumber(0);
+
+// https://stackoverflow.com/questions/16745206/javassist-using-a-jar-file
+//pool.insertClassPath( "/Path/from/root/myjarfile.jar" );
+
+
+//// Execute detection
+//def diffs = traceDiff("H:/Codebase/JGit")
+//diffParser(diffs)
+
+loadFile()
