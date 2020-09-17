46694c8a4ebcd152ccdd70bf6ebb58fa5400782e
Dryan Lam <dunglam@kms-technology.com>
2020-09-18 00:55:31 +0700

    WIP: Detecting method

--- src/ChangeDetectio.groovy
+++ src/ChangeDetectio.groovy
@@ -4,7 +4,7 @@ import javassist.CtClass
 import javassist.LoaderClassPath
 @Grapes([
         @Grab(group = 'org.eclipse.jgit', module = 'org.eclipse.jgit', version = '5.8.1.202007141445-r'),
-        @Grab(group='org.javassist', module='javassist', version='3.27.0-GA')
+        @Grab(group = 'org.javassist', module = 'javassist', version = '3.27.0-GA')
 ])
 
 import org.eclipse.jgit.api.Git
@@ -57,19 +57,18 @@ def RevCommit getHeadCommit(Repository repository) {
 }
 
 
-
-def diffParser(List changes){
+def diffParser(List changes) {
     def diffPattern = /@@(.+?)@@/
 
     def changeScopes = []
-    changes.each{change ->
-        if("MODIFY" == change.type){
+    changes.each { change ->
+        if ("MODIFY" == change.type) {
             def diff = change.diff
             println(diff)
             def detachDiff = (diff =~ diffPattern).findAll()*.last()        // [ -7,9 +7,14 ,  -22,13 +27,29 ]
 
             // Detach changed scopes
-            detachDiff.each{number ->
+            detachDiff.each { number ->
                 def splitChanges = number.split(',')
                 def indxStart = (splitChanges.first() - "-").toInteger()
                 def changeScope = splitChanges.last().toInteger() - 1   // Git always count to unchanged part
@@ -83,34 +82,66 @@ def diffParser(List changes){
     println(changeScopes)
 }
 
-def loadFile(){
+def loadFile() {
     def filePath = "H:/Codebase/JGit/src/main/java/com/dl/jgit/CommitTrace.java"
 
     def lstLines = new File(filePath).readLines()
-    def lstSize = lstLines.size()
-    def lstMethods = lstLines.findAll{it =~ /(public|private|protected|enum) / && !it.contains("class")}
+    def lineSize = lstLines.size()
 
-    def lstIndexMethods = lstLines.findIndexValues{it =~ /(public|private|protected) / && !it.contains("class")}
+    def lstIndexMethods = lstLines.findIndexValues { it =~ /(public|private|protected|enum) / &&
+                                                     !it.contains("class") &&
+                                                     !it.contains("=")}
 
-//    def lstMethods = lstLines.findAll{it =~ /(public|private|protected)/ }.findAll{!it.contains("class")}
+    def methods = lstIndexMethods.collect{lstLines[it].split("\\(").first().split(" ").last()}
+
+    def count = 0
+    for( lineSize; lineSize >= lstIndexMethods.last(); lineSize--){
+        def lineDetect = lstLines[lineSize - 1].trim()
+        def isValid = lineDetect.contains("}") &&
+                !lineDetect.contains("//") &&
+                !lineDetect.contains("/*") &&
+                !lineDetect.startsWith("*")
+        if(isValid){
+            count++
+            if(2 == count){
+                lstIndexMethods += lineSize - 1
+            }
+        }
+    }
+
+    def exp = lstIndexMethods.size() - 2
+
     // [22, 55, 64, lstSize]
-    // above.size()
-    // for i = 1, i <= above.size(); i++
-    def line = lstIndexMethods[i]
+    def result = []
+    for (int i = 0; i <= exp; i++) {
+        def line = lstIndexMethods[i + 1]
+
+        for (line; line > lstIndexMethods[i]; line--) {
+            def lineDetect = lstLines[line].trim()
+            def isValid = lineDetect.contains("}") &&
+                    !lineDetect.startsWith("//") &&
+                    !lineDetect.startsWith("/*") &&
+                    !lineDetect.startsWith("*")
+
+            if (isValid) {
+//                if(i == exp){
+//                    count++
+//                    if(2 == count){
+//                        lstIndexMethods += lineSize - 1
+//                    }
+//                }
+                result += ["method": methods[i], "start": lstIndexMethods[i], "end": line]; break
+            }
+        }
+    }
+    result
+    println()
 
     //
-    // for(line, line > lstIndexMethods[i - 1], line --)
-    def lineDetect = lstLines[line].trim()
-    def checkValid = lineDetect.contains("}")   &&
-                    !lineDetect.contains("//")  &&
-                    lineDetect.toString().startsWith("*")
-
 //    def analyzeList = lstLines[i..j].findAll{!it.contains("//")}
 
     println(lstLines)
 }
-
-
 
 
 //// Execute detection
