3db52870ee27d76550f0254b1a686bf90f086f1d
Dryan Lam <dunglam@kms-technology.com>
2020-09-17 16:04:50 +0700

    Traced diff

--- src/ChangeDetectio.groovy
+++ src/ChangeDetectio.groovy
@@ -1,55 +1,41 @@
-@Grab(group = 'org.eclipse.jgit', module = 'org.eclipse.jgit', version = '5.8.1.202007141445-r')
+@Grapes(
+        @Grab(group = 'org.eclipse.jgit', module = 'org.eclipse.jgit', version = '5.8.1.202007141445-r')
+)
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.diff.DiffFormatter;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.diff.DiffEntry
+import org.eclipse.jgit.diff.DiffFormatter
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-
-def traceDiff() {
-    final String GIT_SOURCE = "H:/Codebase/JGit" + "/.git";
+// Alt + Enter at Grapes to add context libs for IntelliJ
+def traceDiff(String pathDir) {
+    final String GIT_SOURCE = pathDir + "/.git";
     Repository repository = new FileRepositoryBuilder().setGitDir(new File(GIT_SOURCE)).build();
 
-    // Check head with previous one commit
-    RevCommit headCommit = getHeadCommit(repository);
-    RevCommit diffWith = headCommit.getParent(0);
-
-    Map<String, String> diffTrace = new LinkedHashMap<>();
-    List changes = new LinkedList();
-    ByteArrayOutputStream stdout = new ByteArrayOutputStream();
     try {
+        // Check head with previous one commit
+        RevCommit headCommit = getHeadCommit(repository);
+        RevCommit diffWith = headCommit.getParent(0);
+
+        def changes = [];
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
         DiffFormatter diffFormatter = new DiffFormatter(stdout)
         diffFormatter.setRepository(repository);
         List<DiffEntry> entries = diffFormatter.scan(diffWith, headCommit);
+
         for (DiffEntry entry : entries) {
-//                diffFormatter.format(diffFormatter.toFileHeader(entry));
             String filename = entry.getNewPath();
             if (filename.endsWith(".java")) {
                 diffFormatter.format(entry);
-                diffTrace.put("file", entry.getNewPath());
-                diffTrace.put("diff", stdout.toString("UTF-8"));
-                diffTrace.put("type", entry.getChangeType().toString());
-                changes.add(diffTrace);
+                changes += ["file": entry.getNewPath(), "diff": stdout.toString("UTF-8"), "type": entry.getChangeType().toString()]
             }
-
-            println(diffTrace)
         }
+        return changes
     } catch (e) {
+        println("Source-code should have more than one commit. Looks like there is only ONE commit or else. Please check it")
     }
-//    System.out.println(changes);
-//        System.out.println(stdout.toString("UTF-8"));
-//        stdout.reset();
 }
 
 def RevCommit getHeadCommit(Repository repository) {
@@ -61,4 +47,8 @@ def RevCommit getHeadCommit(Repository repository) {
     }
 }
 
-traceDiff()
+
+// Execute detection
+
+def diffs = traceDiff("H:/Codebase/JGit")
+println(diffs.diff)
