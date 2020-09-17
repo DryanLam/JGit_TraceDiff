3b569274730aed9797fe4ef5ece4e03d3e34ce78
Dryan Lam <dunglam@kms-technology.com>
2020-09-17 15:27:50 +0700

    Added changes to List<Map<String,String>

--- src/main/java/com/dl/jgit/CommitTrace.java
+++ src/main/java/com/dl/jgit/CommitTrace.java
@@ -7,9 +7,14 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class CommitTrace {
 
@@ -22,13 +27,29 @@ public class CommitTrace {
         // Check head with previous one commit
         RevCommit headCommit = getHeadCommit(repository);
         RevCommit diffWith = headCommit.getParent(0);
-        FileOutputStream stdout = new FileOutputStream(FileDescriptor.out);
+//        FileOutputStream stdout = new FileOutputStream(FileDescriptor.out);
+
+        Map<String, String> diffTrace = new LinkedHashMap<>();
+        List changes = new LinkedList();
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
         try (DiffFormatter diffFormatter = new DiffFormatter(stdout)) {
             diffFormatter.setRepository(repository);
-            for (DiffEntry entry : diffFormatter.scan(diffWith, headCommit)) {
-                diffFormatter.format(diffFormatter.toFileHeader(entry));
+            List<DiffEntry> entries = diffFormatter.scan(diffWith, headCommit);
+            for (DiffEntry entry : entries) {
+//                diffFormatter.format(diffFormatter.toFileHeader(entry));
+                String filename = entry.getNewPath();
+                if (filename.endsWith(".java")) {
+                    diffFormatter.format(entry);
+                    diffTrace.put("file", entry.getNewPath());
+                    diffTrace.put("diff", stdout.toString("UTF-8"));
+                    diffTrace.put("type", entry.getChangeType().toString());
+                    changes.add(diffTrace);
+                }
             }
         }
+        System.out.println(changes);
+//        System.out.println(stdout.toString("UTF-8"));
+//        stdout.reset();
     }
 
     private static RevCommit getHeadCommit(Repository repository) throws Exception {
